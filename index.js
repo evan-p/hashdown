2ac02e0cc197af893eeda933705febc5f9a1c5bf
petratos <petratos.evan@gmail.com>
2022-10-19 03:45:35 +0300

    escaping-fix

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ const main = () => {
   }
   sections.forEach((s, i) => s.id = i);
   const html_page = fs.readFileSync(__dirname + '/page.html').toString();
-  const html_page_with_sections = html_page.replace('//{sections}', 'const sections = ' + JSON.stringify(sections));
+  const html_page_with_sections = html_page.replace('{{sections}}', encodeURI(JSON.stringify(sections)));
   fs.writeFileSync(`./hashdown.html`, html_page_with_sections);
   return;
 }
